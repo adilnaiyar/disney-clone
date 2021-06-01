f4d7f6d4883d54895423a7555884ac9dd7e7f1dd
adilnaiyar <adil.naiyar@gmail.com>
2021-06-01 17:21:53 +0530

    install firebase, firebase-tools and setup project on firebase also added file firebase.js to setup firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBZr6U4Z9lu3pWXVbnDYu9-Zi-lTqIu3uI",
+    authDomain: "disney-plus-clone-cd4fb.firebaseapp.com",
+    projectId: "disney-plus-clone-cd4fb",
+    storageBucket: "disney-plus-clone-cd4fb.appspot.com",
+    messagingSenderId: "777367825737",
+    appId: "1:777367825737:web:c5a727a7299ce541353e98"
+  };
+  // Initialize Firebase
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+  const provider = new firebase.auth.GoogleAuthProvider();
+  const storage = firebase.storage();
+
+  export { auth, provider, storage };
+  export default db;
