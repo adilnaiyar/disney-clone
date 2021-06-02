0c6fcc0ff18433696f49c2867680fcb706de5c69
adilnaiyar <adil.naiyar@gmail.com>
2021-06-02 18:49:27 +0530

    added viwer.js and style it, added video folder in public and add viwer to home.js

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -1,9 +1,11 @@
 import ImageSlider from "../Image-Slider/imageSlider";
+import Viwers from "../Viwers/viwers";
 import { Container } from "./home.style";
 const Home = () => {
   return (
     <Container>
       <ImageSlider />
+      <Viwers />
     </Container>
   );
 };
